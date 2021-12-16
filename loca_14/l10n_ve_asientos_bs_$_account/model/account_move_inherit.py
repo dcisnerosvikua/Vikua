e8b8731850c881f162630b7b0b3a16397ba301b0
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2021-12-16 11:18:07 -0400

    Modulo tasa

--- loca_14/l10n_ve_asientos_bs_$_account/model/account_move_inherit.py
+++ loca_14/l10n_ve_asientos_bs_$_account/model/account_move_inherit.py
@@ -43,7 +43,7 @@ class AccountMove(models.Model):
                 if lista_tasa:
                     for det in lista_tasa:
                         valor=selff.amount_untaxed_signed*det.rate
-            selff.amount_untaxed_signed_bs=det.rate_real#valor
+            selff.amount_untaxed_signed_bs=det.id#valor
 
     def _compute_monto_conversion_residual(self):
         valor=0
