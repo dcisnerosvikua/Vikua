d0f464e481d61322d6fc9cccbcd2cd7ab394d77f
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2022-01-07 02:36:01 -0400

    xxx

--- loca_14/vat_retention/models/retention_vat.py
+++ loca_14/vat_retention/models/retention_vat.py
@@ -512,6 +512,7 @@ class RetentionVat(models.Model):
         value = {
             'name': name,
             'date': self.move_id.date,#listo
+            'invoice_date':self.move_id.invoice_date,
             #'amount_total':self.vat_retentioned,# LISTO
             'partner_id': self.partner_id.id, #LISTO
             'journal_id':id_journal,
