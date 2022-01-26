ec90ac1500bce6d7b0a6155581b1d6338bfe5eac
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2022-01-26 18:31:18 -0400

    Modulo de campos fechas editables extractos bancarios v2

--- modulos_externos/ext_fechas_editables_extractos/models/account_bank_statement_inherit.py
+++ modulos_externos/ext_fechas_editables_extractos/models/account_bank_statement_inherit.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
 
+from odoo import api, fields, models, _
+from odoo.osv import expression
+from odoo.tools import float_is_zero
+from odoo.tools import float_compare, float_round, float_repr
+from odoo.tools.misc import formatLang, format_date
+from odoo.exceptions import UserError, ValidationError
+
+import time
+import math
+import base64
+
+class AccountBankStatement(models.Model):
+    @api.model
+    def _default_opening_balance(self):
+        #Search last bank statement and set current opening balance as closing balance of previous one
+        journal_id = self._context.get('default_journal_id', False) or self._context.get('journal_id', False)
+        if journal_id:
+            return self._get_opening_balance(journal_id)
+        return 0
 
 class AccountMove(models.Model):
     _inherit = 'account.bank.statement'
