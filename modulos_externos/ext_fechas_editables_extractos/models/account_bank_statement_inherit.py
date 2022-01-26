35903a56e0e3324fb8058f564e5b6563baf0db78
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2022-01-26 18:32:40 -0400

    Modulo de campos fechas editables extractos bancarios v3

--- modulos_externos/ext_fechas_editables_extractos/models/account_bank_statement_inherit.py
+++ modulos_externos/ext_fechas_editables_extractos/models/account_bank_statement_inherit.py
@@ -25,3 +25,11 @@ class AccountMove(models.Model):
 
     balance_start = fields.Monetary(string='Starting Balance', states={'confirm': [('readonly', False)]}, default=_default_opening_balance)
     balance_end_real = fields.Monetary('Ending Balance', states={'confirm': [('readonly', False)]})
+
+    @api.model
+    def _default_opening_balance(self):
+        #Search last bank statement and set current opening balance as closing balance of previous one
+        journal_id = self._context.get('default_journal_id', False) or self._context.get('journal_id', False)
+        if journal_id:
+            return self._get_opening_balance(journal_id)
+        return 0
