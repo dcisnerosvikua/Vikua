0c42be54f71e639409ca453ffacc777d5035c778
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2022-03-10 10:48:21 -0400

    Modulo oscar

--- modulos_externos/l10n_usd_account_report/__manifest__.py
+++ modulos_externos/l10n_usd_account_report/__manifest__.py
@@ -5,17 +5,14 @@
     'version': '1.1',
     'author': 'INM & LDR Soluciones Tecnológicas y Empresariales C.A',
     'website': 'http://soluciones-tecno.com/',
-    #'category': 'Accounting/Localizations/Reporting',
     'category': 'Tools',
     'version': '0.1',
     'depends': ['account'],
     'data': [
         'report/paperformat.xml',
         'report/ir_action_report.xml',
-        #'views/account_move_views.xml',
         'views/report_invoice_fact.xml',
-        #'views/report_account_invoice_delivery_note.xml',
     ],
-    'application': True,
+    'application': False,
     'auto_install': False,
 }
