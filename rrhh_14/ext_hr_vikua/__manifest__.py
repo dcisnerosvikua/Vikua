ba26b932b213e3d4ca616cfae8e46b64006d2a9e
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2022-03-31 14:49:52 -0400

    Modulo extension de rrhh para vikua

---
+++ rrhh_14/ext_hr_vikua/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Contract Aditional Data',
+    'version': 'odoo 14',
+    'author': 'INM&RDL - Ing Darrell Sojo',
+    'category': 'Human Resources',
+    'sequence': 104,
+    'summary': 'Adds aditional fields to hr_contract module',
+    'description': '''
+        Agrega  campos y fuancionalidades adicionales partivulares al modulo rrhh:\n
+            
+    ''',
+    'depends': [
+        'base_setup',
+        'base',
+        'hr',
+        'hr_contract',
+        'hr_payroll',
+        'hr_attendance',
+        'hr_holidays',
+        'hr_holidays',
+        'hr_campos_parametrizacion',  ## este es un modulo de odoo nativo para que haga el asiento de la nomina
+    ],
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [],
+}
