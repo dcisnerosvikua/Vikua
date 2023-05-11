bbb3af957dbcd3e5549ad2b879a3ff8b51870492
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2023-05-11 16:51:11 -0400

    libro v3

--- loca_14/libros_filtros/model/mymodules.py
+++ loca_14/libros_filtros/model/mymodules.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models, api, exceptions
+from odoo import fields, models, api, exceptions,_
 import logging
 from odoo.exceptions import UserError
 
