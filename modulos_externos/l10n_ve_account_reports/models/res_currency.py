e07c6dea236e6fc125717f8a8de5838b4ab0506a
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2023-10-10 14:21:20 -0400

    limpieza

--- modulos_externos/l10n_ve_account_reports/models/res_currency.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import fields, models
-
-
-class ResCurrency(models.Model):
-    _inherit = 'res.currency'
-
-    long_name = fields.Char(string='Nombre Completo')
--- rrhh_14/ext_hr_vikua/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-from . import ext_hr_payslip
-from . import hr_contract_add_fields
-from . import hr_payroll_structure_inherit
