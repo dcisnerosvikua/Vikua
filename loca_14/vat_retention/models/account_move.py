573ecd301e6ae69b12c2b06df2de6eae324f47c5
superka86 <95897252+superka86@users.noreply.github.com>
2024-01-17 09:50:14 -0400

    Correccion de error en llamada de datetime.date()
    Se cambio la linea de importacion de datetime

--- loca_14/vat_retention/models/account_move.py
+++ loca_14/vat_retention/models/account_move.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import logging
-from datetime import datetime
+#from datetime import datetime
+import datetime
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
 
